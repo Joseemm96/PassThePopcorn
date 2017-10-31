8c55f1b04f1ec9858a0ed9adcf685996c2affd73
Jose Mavarez <joseemm96@gmail.com>
2017-10-31 18:14:30 -0400

    ultimo cambio

--- js/buscador.js
+++ js/buscador.js
@@ -78,3 +78,46 @@ function getMovie(){
       console.log(error);
     });
 }
+
+
+
+//Funcion de validar
+
+function validar (){
+  var  nombre, email, expresion, texto;
+  nombre = document.getElementById("nombre").value;
+  email = document.getElementById("email").value;
+  texto = document.getElementById("message").value;
+
+  expresion = /\w+@\w+\.+[a-z]/;
+
+
+  if(nombre === ""){
+    alert("El campo nombre está vacío");
+    return false;
+  }
+  else if(email === ""){
+    alert("El campo correo está vacío");
+    return false;
+  }
+  else if(texto === ""){
+    alert("El campo de texto está vacío")
+    return false;
+    }
+  else if(nombre.length>30){
+    alert("El nombre es muy largo");
+    return false;
+  }
+  else if(correo.length>100){
+    alert("El correo es muy largo");
+    return false;
+  }
+  else if(texto.length<4){
+    alert("Mensaje muy corto");
+    return false;
+  }
+  else if(!expresion.text(correo)){
+    alert("El correo tiene un formato válido")
+    return false;
+  }
+}
